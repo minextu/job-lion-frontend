659df219ea3d24a1a9b4e35e7f4fe515172c3aa0
Justus <justus.renkhoff@gmx.de>
2018-01-03 19:51:36 +0100

    Upload New File

---
+++ menu.inc.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="de">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menue</title>
+	<link rel="stylesheet" type="text/css" href="style.css" >
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+	<script language="javascript" type="text/javascript" src="nav.js"></script>
+  </head>
+  <body>
+	<div class="topNav" id="myTopNav">
+		<a href="index.php" data-role="ajax">Startseite</a>
+		<a href="kategorien.php" data-role="ajax">Kategorien</a>
+		<a href="erfahrungsberichte.php" data-role="ajax">Erfahrungsberichte</a>
+		<a href="impressum.php" data-role="ajax">Impressum</a>
+		<a href="anmeldung.php" data-role="ajax">Anmelden</a>
+		<a href="javascript:void(0);" class="icon" onclick="nav()">&#9776;</a>
+	</div>
+	<script src="script.js"></script>
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+	<script src="https://unpkg.com/scrollreveal/dist/scrollreveal.min.js"></script>
+  </body>
+</html>
