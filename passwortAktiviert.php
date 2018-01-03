1e62b1bec63af25eb90437380aff395452c9697d
Justus <justus.renkhoff@gmx.de>
2018-01-03 19:51:50 +0100

    Upload New File

---
+++ passwortAktiviert.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="de">
+	<head>
+		<title>Passwort aktiviert</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+		<link rel="stylesheet" type="text/css" href="style.css" >
+		<script language="javascript" type="text/javascript" src="nav.js"></script>
+	</head>
+	<body>
+		<header>
+			<?php include("menu.inc.php"); ?>
+		</header>
+		<main>
+			<h1 style="padding: 3em 0 1em 0; text-align: center;">Account erfolgreich aktiviert!</h1>
+			<p  style="padding: 0em 0 2em 0; text-align: center;"><a href="index.php">Zurück zu Job &bull; Lion</a></p>
+		</main>
+		<footer>
+			<?php include("footer.inc.php"); ?>
+		</footer>
+		<script src="script.js"></script>
+		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+		<script src="https://unpkg.com/scrollreveal/dist/scrollreveal.min.js"></script>
+	</body>
+</html>
