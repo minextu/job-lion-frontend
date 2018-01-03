c8816516895fb3c4691b855cdc3277fbabbbc003
Justus <justus.renkhoff@gmx.de>
2018-01-03 19:51:00 +0100

    Upload New File

---
+++ impressum.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="de">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Impressum</title>
+	
+	  <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="style.css" >
+	<script language="javascript" type="text/javascript" src="nav.js"></script>
+  </head>
+  <body>
+	<?php include ("menu.inc.php"); ?>
+	<div id="impressum">
+		<h1>Impressum</h1>
+		<h2>ANGABEN GEMÄß § 5 TMG: </h2>
+		<p>Nagel Bohr Renkhoff GbR </p>
+		<p>
+			Neubrücker Straße 9928 <br>
+			Am Umwelt-Campus Birkenfeld <br>
+			z.H. Herrn Oliver Nagel <br>
+			55768 Hoppstädten-Weiersbach 
+		</p>
+		<h2>VERTRETEN DURCH:</h2>
+		<p>Oliver Nagel, Hannah Bohr, Justus Renkhoff <br>
+			Neubrücker Straße. 9928 <br>
+			Am Umwelt-Campus Birkenfeld <br>
+			55768 Hoppstädten-Weiersbach</p>
+		<h2>KONTAKT:</h2>
+		<p>E-Mail: info@job-lion.com</p>
+		<p>© Bildrechte: fotolia.com</p>
+	</div>
+	<footer>
+	  	<?php include("footer.inc.php"); ?>
+	</footer>
+	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+	<script src="script.js"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+	<script src="https://unpkg.com/scrollreveal/dist/scrollreveal.min.js"></script>
+  </body>
+</html>
