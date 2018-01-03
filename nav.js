8da3041f9502af79e42a463b218fe34ccd0bf45f
Justus <justus.renkhoff@gmx.de>
2018-01-03 19:53:19 +0100

    Upload New File

---
+++ nav.js
@@ -0,0 +1,8 @@
+function nav() {
+    var x = document.getElementById("myTopNav");
+    if (x.className === "topNav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topNav";
+    }
+}
